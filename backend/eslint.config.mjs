4e2b5db42dd36c6818a2dc182a1a8f5311c7976b
Arturhk05 <arturhandow@gmail.com>
2025-11-18 23:38:14 -0300

    backend(chore): add eslint and prettier

---
+++ backend/eslint.config.mjs
@@ -0,0 +1,15 @@
+// @ts-check
+
+import eslint from "@eslint/js"
+import { defineConfig } from "eslint/config"
+import tseslint from "typescript-eslint"
+import prettierConfig from "eslint-config-prettier"
+
+export default defineConfig(
+  {
+    ignores: ["dist/**"],
+  },
+  eslint.configs.recommended,
+  tseslint.configs.recommended,
+  prettierConfig
+)
